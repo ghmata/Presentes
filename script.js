0f0807be144e906aee5e823e9ed9e49380e47b1d
ghmata <gabrielhm00@gmail.com>
2025-05-30 20:40:53 -0300

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-// Parallax effect
-window.addEventListener('scroll', function() {
-  const scrolled = window.scrollY;
-  const parallax = document.querySelector('.parallax-bg');
-  if (parallax) {
-    parallax.style.transform = 'translateY(' + scrolled * 0.28 + 'px)';
-  }
-});
